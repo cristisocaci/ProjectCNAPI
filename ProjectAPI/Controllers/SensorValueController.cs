622f31aa4040800ce08e71270d6d65d3c52d339d
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-11-28 15:02:50 +0200

    init

---
+++ ProjectAPI/Controllers/SensorValueController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SensorValueController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int value)
+        {
+            return Ok(value);
+        }
+    }
+}
